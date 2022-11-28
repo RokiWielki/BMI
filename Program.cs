f363670547d6b0848b83f832c5bc4e669a1a4b6d
RokiWielki <Cruck222@interia.pl>
2022-11-28 17:36:07 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Napisz program, który oblicza wskaźnik masy ciała BMI. Program ma prosić
+//użytkownika o podanie wagi w kilogramach oraz wzrostu w metrach.
+//Wzór: BMI= masa/wzrost^2
+
+using System;
+
+namespace BMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your BMI?");
+            Console.Write("(KG) Weight: ");
+            double weight=double.Parse(Console.ReadLine());
+            Console.Write(" (M) Height: ");
+            double height=double.Parse(Console.ReadLine());
+            Console.WriteLine("Your BMI: {0,0:F2}",BMI(weight,height));
+        }
+        static double BMI(double w, double h)
+        {
+            double result = w / (h * h);
+            return result;
+        }
+    }
+}
